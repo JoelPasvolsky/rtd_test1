e1e37cd2108e8a5bc800d645abccd91c6daa858b
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2026-01-20 13:18:09 -0800

    Tes with sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -43,44 +43,12 @@ copybutton_line_continuation_character = "\\"
 if os.environ.get('READTHEDOCS', False):
     pass
 
-html_theme = 'pydata_sphinx_theme'
+html_theme = 'sphinx_rtd_theme'
 html_logo = "_static/test.png"
 
 copyright = 'D-Wave'
 version = "0.1"
 
-html_theme_options = {
-    "footer_start": ["copyright"],
-#    "footer_end": ["version"],
-    "icon_links_label": "Quick Links",
-    "icon_links": [
-        {
-            "name": "GitHub",
-            "url": "https://github.com/JoelPasvolsky/rtd_test1",
-            "icon": "fa-brands fa-square-github",
-            "type": "fontawesome",
-            "attributes": {
-                "aria-label": "github repo link",
-            },
-        },
-    ],
-    "collapse_navigation": True,
-    "header_links_before_dropdown": 5,
-    "navbar_align": "left",
-    "show_prev_next": False,
-    "logo": {
-        "image_light": "_static/test.png",
-        "image_dark": "_static/test.png",
-    },
-    "secondary_sidebar_items": [
-        "page-toc",
-        "page-source",
-    ],
-    "navbar_end": ["navbar-icon-links"],
-}
-html_sidebars = {
-    "**": ["sidebar-nav-bs"],  # remove ads
-}
 html_static_path = ['_static']
 templates_path = ['_templates']
 
